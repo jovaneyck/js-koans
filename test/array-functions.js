6cbdb333351add38d3b307011118220018d53eae
Jo Van Eyck <jo.vaneyck@ae.be>
2017-11-22 16:09:24 +0100

    map exercise

--- test/array-functions.js
+++ test/array-functions.js
@@ -1,9 +1,11 @@
 let should = require('chai').should();
-
+let TODO = []
 describe("Arrays", () => {
     describe("Map", () => {
         it("Applies any function over all elements", () =>{
-            (1+3).should.equal(4)
+            let input = [2,3,4,5]
+            let triples = TODO
+            triples.should.eql([6,9,12,15])
         })
     })
 })
