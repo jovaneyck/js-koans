835112e8a9ebfc1fd7114e002c6c9659d660fbf0
Jo Van Eyck <jo.vaneyck@ae.be>
2017-11-22 15:35:57 +0100

    hello world test

---
+++ test/array-functions.js
@@ -0,0 +1,9 @@
+let should = require('chai').should();
+
+describe("Arrays", () => {
+    describe("Map", () => {
+        it("Applies any function over all elements", () =>{
+            (1+3).should.equal(4)
+        })
+    })
+})
