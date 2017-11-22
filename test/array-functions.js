a09c8dc2cb71fbdc1cab8f9cb7b2ac83d7bf746f
Jo Van Eyck <jo.vaneyck@ae.be>
2017-11-22 16:14:19 +0100

    basic filter example

--- test/array-functions.js
+++ test/array-functions.js
@@ -4,8 +4,32 @@ describe("Arrays", () => {
     describe("Map", () => {
         it("Applies any function over all elements", () =>{
             let input = [2,3,4,5]
+
             let triples = TODO
+
             triples.should.eql([6,9,12,15])
+        })
+    })
+
+    describe("Filter", () => {
+        it("Can filter elements", () => {
+            let input = [1,2,3,4,5,6]
+            let isEven = TODO
+
+            let evens = input.filter(isEven)
+
+            evens.should.eql([2,4,6])
+        })
+
+        it("Can filter out irrelevant elements using any given predicate", () => {
+            let users = [
+                { name: "Bob", paid: false},
+                { name: "Alice", paid: true}
+            ]
+
+            let paidUsers = TODO
+
+            paidUsers.should.eql([{name: "Alice", paid: true}])
         })
     })
 })
