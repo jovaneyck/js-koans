e98c2927d731d22f14e156a47880d19d7d754520
Jo Van Eyck <jo.vaneyck@ae.be>
2017-11-27 18:19:30 +0100

    fully covered statement function with tests

--- test/refactoring-statement.js
+++ test/refactoring-statement.js
@@ -16,6 +16,70 @@ describe("statement function", () => {
             "You earned 1 frequent renter points\n"
         s.should.be.equal(expected)
     })
-})
 
-describe("Full acceptance test", () => {})
+    it("A regular movie rented for more than two days", () => {
+        let s = statement(
+            {
+                name:"Bob", 
+                rentals: [{movieID: 3, days: 3}]},
+            { "3" : {title: "a movie", code : "regular"}})
+        let expected = 
+            "Rental Record for Bob\n"+
+            "\ta movie\t3.5\n"+
+            "Amount owed is 3.5\n"+
+            "You earned 1 frequent renter points\n"
+        s.should.be.equal(expected)
+    })
+
+    it("A new movie rented for more than two days", () => {
+        let s = statement(
+            {
+                name:"Bob", 
+                rentals: [{movieID: 3, days: 3}]},
+            { "3" : {title: "a movie", code : "new"}})
+        let expected = 
+            "Rental Record for Bob\n"+
+            "\ta movie\t9\n"+
+            "Amount owed is 9\n"+
+            "You earned 2 frequent renter points\n"
+        s.should.be.equal(expected)
+    })
+
+    it("A children's movie rented for four days", () => {
+        let s = statement(
+            {
+                name:"Bob", 
+                rentals: [{movieID: 3, days: 4}]},
+            { "3" : {title: "a movie", code : "childrens"}})
+        let expected = 
+            "Rental Record for Bob\n"+
+            "\ta movie\t3\n"+
+            "Amount owed is 3\n"+
+            "You earned 1 frequent renter points\n"
+        s.should.be.equal(expected)
+    })
+
+    describe("Full acceptance test", () => {
+        it("should keep working", () => {
+            let s = statement(
+                {
+                    name:"Alice", 
+                    rentals: [
+                        {movieID: 2, days: 3},
+                        {movieID: 3, days: 4},
+                        {movieID: 4, days: 5}]},
+                {   "2" : {title: "a regular movie", code : "regular"},          
+                    "3" : {title: "a new movie", code : "new"},          
+                    "4" : {title: "a childrens movie", code : "childrens"}})
+            let expected = 
+                "Rental Record for Alice\n"+
+                "\ta regular movie\t3.5\n"+
+                "\ta new movie\t12\n"+
+                "\ta childrens movie\t4.5\n"+
+                "Amount owed is 20\n"+
+                "You earned 4 frequent renter points\n"
+    
+            s.should.be.equal(expected)
+        })
+    })
+})
